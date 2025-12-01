22873622b0d4b64e9f971c150a1c5ccff8b23fad
SWIPAX-fantasy <unai.fernandez.cobos@estudiantat.upc.edu>
2025-12-01 16:41:41 +0100

    arenasbotonOK

--- src/App.jsx
+++ src/App.jsx
@@ -4,30 +4,27 @@ import Header from "./components/Header";
 import Footer from "./components/Footer";
 import HomePage from './pages/HomePage';
 import CardDetail from "./pages/CardDetail";
+import Arenas from "./pages/Arenas"; // ← IMPORTANTE
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-
-
-
 
 export default function App() {
   return (
-    <div className="app-layout">
-      
-      <main>
-        
-        <BrowserRouter>
-          <Header />
+    <BrowserRouter>
+      <div className="app-layout">
+
+        <Header />
+
+        <main>
           <Routes>
             <Route path="/" element={<HomePage />} />
             <Route path="/personaje/:id" element={<CardDetail />} />
-            
+            <Route path="/arenas" element={<Arenas />} /> {/* ← AQUÍ VA */}
           </Routes>
-          <Footer />
-        </BrowserRouter>
-        
-      </main>
+        </main>
 
-      
-    </div>
+        <Footer />
+
+      </div>
+    </BrowserRouter>
   );
 }
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,28 +1,30 @@
 import React from 'react';
 import './Footer.css';
+import { useNavigate } from "react-router-dom";
 
-// IMPORTA TUS IMÁGENES AQUÍ
-// Asegúrate de que los nombres coincidan con los que guardaste en src/assets
+// Importa tus imágenes
 import imgCartas from '../assets/icono_cartas.png';
 import imgMazo from '../assets/icono_mazo.png';
 import imgBatalla from '../assets/icono_batalla.png';
 
 export default function Footer() {
+  const navigate = useNavigate();
+
   return (
     <footer className="footer">
       
-      {/* Botón 1: Cartas (Activo por defecto como ejemplo) */}
+      {/* Botón 1: Cartas */}
       <button className="footer-btn active">
         <img src={imgCartas} alt="Cartas" className="footer-icon-img" />
       </button>
-      
-      {/* Botón 2: Mazo/Grid */}
+
+      {/* Botón 2: Mazo */}
       <button className="footer-btn">
         <img src={imgMazo} alt="Mazo" className="footer-icon-img" />
       </button>
-      
-      {/* Botón 3: Batalla/Estadio */}
-       <button
+
+      {/* Botón 3: Arenas */}
+      <button
         className="footer-btn"
         onClick={() => navigate("/arenas")}
       >
